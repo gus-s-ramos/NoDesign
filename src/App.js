7f6e77be114d37397ce3e4b3557e53d0a0fcbd9b
gus-s-ramos <gustavorabr@gmail.com>
2024-03-05 02:35:21 -0300

    Alterado tela de login, e corrigido alguns bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import './App.css';
 import Navbar from './components/navbar';
 import Gamification from './pages/gamification';
@@ -7,12 +7,25 @@ import Layout from "./pages/layout";
 import Signature from "./pages/signature";
 import Login from "./pages/login";
 
-
 function App() {
   return (
-
     <Router>
-      <Navbar />
+      <AppContent />
+    </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+
+  // Verifica se a rota atual é "/login"
+  const isLoginPage = location.pathname === "/login";
+
+  return (
+    <>
+      {/* Renderiza o Navbar se a rota atual não for "/login" */}
+      {!isLoginPage && <Navbar />}
+      
       <Routes>
         <Route path="/gamification" element={<Gamification />} />
         <Route path="/report" element={<Report />} />
@@ -20,7 +33,7 @@ function App() {
         <Route path="/signature" element={<Signature />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
